95ebe683b5d3e948c598de98934fef57adb8838f
Rachel Lin <rachel.lin91@gmail.com>
2015-08-04 12:26:06 +0800

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'themagicsatchel.heroku.com' }
+
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 end
